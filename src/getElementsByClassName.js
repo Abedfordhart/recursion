0e242d639904d3f37682e35ddb441324ddb35064
Andrew Bedford-Hart <abedfordhart@gmail.com>
2017-03-16 13:01:07 -0700

    Passing tests for getElementsByClassName.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,18 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className, parent, results){
   // your code here
-};
+  var results = results || [];
+  var parent = parent || document.body;
+
+  if(parent.classList.contains(className)){
+    results.push(parent);
+  }
+
+  for(var i = 0; i < parent.children.length; i++){
+    results.concat(getElementsByClassName(className, parent.children[i], results));
+  }
+
+  return results;
+}
